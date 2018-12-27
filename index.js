296f44d90acb5de5b5984b2db48d0313142e7992
Tim Flood <tflood9@gmail.com>
2018-12-27 15:00:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromObjectByKey(object, key){
 
 function destructivelyDeleteFromObjectByKey(object, key){
   var newObject = obj;
-  delete obj;
+  delete obj.newObject;
   delete recipes.key;
   return recipes;
 }
